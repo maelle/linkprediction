b77a88757a3ddb4d3cac63dbb6719e9d190738ec
Maëlle Salmon <maelle.salmon@yahoo.se>
2024-10-07 10:15:05 +0200

    chore: replace deprecated `igraph::similarity.dice()`

--- R/similarity_sor.R
+++ R/similarity_sor.R
@@ -4,10 +4,10 @@
 #
 # This function measures a relative size of an intersection of neighbors sets
 #   of two vertices. It is a wrapper of an \pkg{igraph} function
-#   \code{\link[igraph]{similarity.dice}}.
+#   \code{\link[igraph]{similarity}} with \code{method="dice"}.
 #
 
 similarity_sor <- function(graph, v1, v2, ...){
-  score <- igraph::similarity.dice(graph)
+  score <- igraph::similarity(graph, method = "dice")
   score[v1, v2]
 }
